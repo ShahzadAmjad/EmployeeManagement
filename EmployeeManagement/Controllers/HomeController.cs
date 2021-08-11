b61a4c041050a2d67adae3f3b18f95c624b81b3e
Administrator <shahzadamjad94@gmail.com>
2021-08-11 09:38:03 +0500

    Add project files.

---
+++ EmployeeManagement/Controllers/HomeController.cs
@@ -0,0 +1,60 @@
+﻿using EmployeeManagement.Models;
+using EmployeeManagement.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeManagement.Controllers
+{
+    //[Route("[controller]")]
+    public class HomeController: Controller
+    {
+        private IEmplyeeRepositry _emplyeeRepositry;
+        public HomeController(IEmplyeeRepositry emplyeeRepositry)
+        {
+            _emplyeeRepositry = emplyeeRepositry;
+        }
+        //[Route ("")]
+        //[Route("[action]")]
+        //[Route("~/")]
+        public ViewResult Index() 
+        {
+            var model= _emplyeeRepositry.GetAllEmployee();
+            return View(model);
+        }
+
+        //[Route("[action]/{id?}")]
+        public ViewResult Details(int? id)
+        {
+            HomeDetailsViewModel homeDetailsViewModel = new HomeDetailsViewModel() 
+            {
+                Employee = _emplyeeRepositry.GetEmployee(id??1), 
+                PageTitle= "Employee Details" 
+            };
+           
+            //Employee model= _emplyeeRepositry.GetEmployee(1);
+            //ViewBag.PageTitle = "Employee Details";
+            //ViewBag.Employee = model;
+            
+            return View(homeDetailsViewModel);
+        }
+        [HttpGet]
+        public ViewResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Employee employee)
+        {
+            if (ModelState.IsValid) { 
+            Employee newEmployee= _emplyeeRepositry.Add(employee);
+            //return View();
+            return RedirectToAction("details", new { id = newEmployee.Id });
+            }
+            return View();
+        }
+
+    }
+}
---
+++ EmployeeManagement/Models/Employee.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeManagement.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(50, ErrorMessage ="Name cannot increse 50 char")]
+        public string Name { get; set; }
+        [Required]
+        [RegularExpression(@"^([\w\.\-]+)@([\w\-]+)((\.(\w){2,3})+)$", ErrorMessage ="Invalid Email")]
+        
+        public string Email { get; set; }
+
+        public Dept Department { get; set; }
+    }
+}
